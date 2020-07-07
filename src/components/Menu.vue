4735bdba55402bcb673b4c9f838bd56771cc5854
Ones <964981348@qq.com>
2020-07-07 09:44:26 +0800

    响应式搭建完成

---
+++ src/components/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-menu default-active="/" :mode="mode" @select="handleSelect" router>
+    <el-menu-item index="/">首页</el-menu-item>
+    <el-menu-item index="/news">新闻资讯</el-menu-item>
+    <el-menu-item index="/product">产品中心</el-menu-item>
+    <el-menu-item index="/case">经典案例</el-menu-item>
+    <el-menu-item index="/goin">走进科建</el-menu-item>
+    <el-menu-item index="/download">下载APP</el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  props: {
+    mode: {
+      type: String
+    },
+    handleSelect: {
+      type: Function
+    }
+  }
+};
+</script>
+
+<style>
+</style>
